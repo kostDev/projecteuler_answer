b1bbb1fe5e795a09c4d76c90bb8a62f1b6a572dd
Godson <kostyaboy777@gmail.com>
2017-10-01 20:33:07 +0300

    done 14

--- task11-20/task14.js
+++ task11-20/task14.js
@@ -1,1 +1,19 @@
-let answer = 0;
+let limit = 1e6, number = 14, countTerms = 1, temp = 0,
+     high = {count: 0, number: 0};
+let even = n => n / 2;
+let odd  = n => 3 * n + 1;
+console.time('code time');
+for( ; number <= limit; number++){
+    temp = number;
+    while(temp != 1){
+        temp % 2 == 0 ? temp = even(temp) : temp = odd(temp);
+        ++countTerms;
+    }
+    if(countTerms > high.count){
+        high.count = countTerms;
+        high.number = number;
+    }
+    countTerms = 1;
+}
+console.timeEnd('code time');
+console.log(high);
