694b069fd6f648751c91b08ee310b5f05bd1e3f4
Godson <kostyaboy777@gmail.com>
2017-10-01 16:19:33 +0300

    do 12 task

--- task12.js
+++ task12.js
@@ -1,16 +1,13 @@
-let findTriangleNumber = () =>{
-	let number = 8, countDivisors = 2, temp = 0;//, count  // 2 3 5 7
-	while(true){
-		temp = (number * (number + 1)) / 2;
-		
+let findTriangleNumber = limitOfDivisors =>{
+	let value = 0, number = 0, numberOfDivisors = 0;
+	while(numberOfDivisors <= limitOfDivisors){
+		numberOfDivisors = 0;
+		value = (number * (number + 1)) / 2;
+		for (let i = 1; i <= Math.sqrt(value); i++)
+            if (value % i == 0) numberOfDivisors++;
+        numberOfDivisors *= 2;
+        ++number;
 	}
-	console.log('number: ' + number + ' temp: '+ temp + ' countDi: ' + countDivisors);
-
+	console.log(value);
 }
-// let showDivisorsOfNumber = num =>{
-// 	let divisor = 0, countPrime2 = 0, countPrime3 = 0, countPrime5 = 0, countPrime7 = 0;
-// 	if(Math.sqrt(num) == parseInt(Math.sqrt(num))){
-
-// 	}
-// }
-findTriangleNumber();
+findTriangleNumber(500);
